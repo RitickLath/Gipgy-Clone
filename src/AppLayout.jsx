1903dcd670b21dcf66f60302544fc927cac58808
RitickLath <ritick943lath@gmail.com>
2024-05-06 02:37:09 +0530

    Navbar UI Static

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className="bg-[#121212] pt-6 px-28 text-white w-full h-screen">
       <Navbar />
       <Outlet />
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import GIPHY from "../assets/GIPHY.png";
+import { IoMdMore } from "react-icons/io";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [showDiv, setShowDiv] = useState(false);
+
+  return (
+    <div className="mb-8  relative">
+      <div className=" flex justify-around">
+        <Link to="/">
+          <img className="w-[150px]" src={GIPHY} alt="" />
+        </Link>
+
+        {/* Dynamic Take 4 Category */}
+        <Link className="hidden lg:block mt-2">Category-1</Link>
+        <Link className="hidden lg:block mt-2">Category-1</Link>
+        <Link className="hidden lg:block mt-2">Category-1</Link>
+        <Link className="hidden lg:block mt-2">Category-1</Link>
+        <button onClick={() => setShowDiv(!showDiv)}>
+          <IoMdMore size={25} className="mt-3" />
+        </button>
+        {/* Favourite */}
+        <Link to="/favourite" className="hidden lg:block mt-2">
+          Favourite
+        </Link>
+      </div>
+      {showDiv && (
+        <div className="w-full bg-yellow-100 lg:h-[200px] mt-3"></div>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
