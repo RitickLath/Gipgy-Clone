dc24481eabdedadbb6bd46ecdca626f9a65b76ac
RitickLath <ritick943lath@gmail.com>
2024-05-07 16:47:54 +0530

    made buttons for GIFs, Stickers and Clips with their corresponding State

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import { GifState } from "../context/Context";
 
 const HomePage = () => {
-  const { gf } = GifState();
+  const { gf, filter, setFilter } = GifState();
   const [arr, setArr] = useState([]);
+  const [selected, setSelected] = useState("GIFs");
+
   const [hover, setHover] = useState(null);
 
   useEffect(() => {
@@ -23,24 +25,56 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="flex flex-wrap px-8">
-      {arr.map((a, index) => (
-        <div className="relative" key={a.images}>
-          <img
-            className="relative"
-            style={{ marginRight: "10px", marginBottom: "10px" }}
-            src={a.images.fixed_width.webp}
-            alt=""
-            onMouseEnter={() => setHover(index)}
-            onMouseLeave={() => setHover(null)}
-          />
-          {hover == index && (
-            <div className="text-lg font-bold absolute top-5 left-3">
-              {a.username}
-            </div>
-          )}
+    <div>
+      <div className="flex md:justify-end lg:mr-4">
+        <div className="w-full flex justify-around md:w-[60%] lg:w-[40%] bg-[#2E2E2E] rounded-xl">
+          <button
+            onClick={() => setSelected("GIFs")}
+            className={`text-lg rounded-xl font-semibold flex-1 py-2 ${
+              selected === "GIFs" ? "bg-gray-600" : ""
+            }`}
+          >
+            GIFs
+          </button>
+          <button
+            onClick={() => setSelected("Stickers")}
+            className={`text-lg rounded-xl font-semibold flex-1 py-2 ${
+              selected === "Stickers" ? "bg-gray-600" : ""
+            }`}
+          >
+            Stickers
+          </button>
+          <button
+            onClick={() => setSelected("Clips")}
+            className={`text-lg rounded-xl font-semibold flex-1 py-2 ${
+              selected === "Clips" ? "bg-gray-600" : ""
+            }`}
+          >
+            Clips
+          </button>
         </div>
-      ))}
+      </div>
+      <div className="flex flex-wrap px-8 mt-10">
+        {arr.map((a, index) => (
+          <div className="relative" key={a.id}>
+            <img
+              className="relative group"
+              style={{ marginRight: "10px", marginBottom: "10px" }}
+              src={a.images.fixed_width.webp}
+              alt=""
+              onMouseEnter={() => setHover(index)}
+              onMouseLeave={() => setHover(null)}
+            />
+            {hover == index && (
+              <div className="text-lg flex space-x-5 font-bold  absolute top-5 left-3">
+                {/* <img src={a.user.avatar_url} alt="" /> */}
+                <img className="w-[20px]" src={a?.user?.avatar_url} alt="" />
+                {a?.username}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
